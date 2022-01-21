18c1247c07781fbd6d49b511547b60c5d2649694
Alex <19alexferrante99@gmail.com>
2022-01-21 23:49:55 +0100

    add jwt validation helper

---
+++ src/helpers/JWT.validation.ts
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import config from "../config/config";
+
+const generateToken = (secret: string, expiresIn: string) => {
+  return (id: string) => {
+    return jwt.sign(id, secret, { expiresIn });
+  }
+}
+
+const validateToken = (secret: string) => {
+  return (token: string) => {
+    return jwt.verify(token, secret);
+  }
+}
+
+export const generateAccessToken = 
+generateToken(config.accessToken.secret, config.accessToken.expiry);
+
+export const validateAccessToken =
+validateToken(config.accessToken.secret);
+
+export const generateRefreshToken =
+generateToken(config.refreshToken.secret, config.refreshToken.expiry);
+
+export const validateRefreshToken =
+validateToken(config.refreshToken.secret);
