97f2f6f4149a317c0363f41a046d41176a8f382f
Alex <19alexferrante99@gmail.com>
2022-01-22 19:32:00 +0100

    update email with promisify

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,5 +1,7 @@
 import nodemailer from "nodemailer";
 import config from "../config/config";
+import util from "util";
+
 
 const transporter = nodemailer.createTransport({
   service: config.email.service,
@@ -9,16 +11,17 @@ const transporter = nodemailer.createTransport({
   }
 });
 
+const sendMail = util.promisify(transporter.sendMail).bind(transporter);
 
 export const sendVerificationEmail = async (
   email: string, token: string
-  ): Promise<void> => {
-    const url = `http://${config.server.host}:${config.server.port}/verify/${token}`;
-    const emailOptions = {
-      from: config.email.user,
-      to: email,
-      subject: "Email verification.",
-      html: `<p>Click this <a href=${url}>link</a> to verify email.`
-    }
-    await transporter.sendMail(emailOptions);
+): Promise<void> => {
+  const url = `http://${config.server.host}:${config.server.port}/verify/${token}`;
+  const emailOptions = {
+    from: config.email.user,
+    to: email,
+    subject: "Email verification.",
+    html: `<p>Click this <a href=${url}>link</a> to verify email.`
+  }
+  await sendMail(emailOptions);
 }
