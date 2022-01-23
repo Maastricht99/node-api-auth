410f397502834314c94a8e17e8f5b3a28ed4056a
Alex <19alexferrante99@gmail.com>
2022-01-23 15:17:50 +0100

    remove whitespaces

--- src/controllers/test.controller.ts
+++ src/controllers/test.controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import { IUserRequest } from "../interfaces/interfaces";
 
-
 export const servePublic = async (
   req: Request, res: Response
 ): Promise<Response> => {
